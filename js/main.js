35cf14c32b8aa237757eb81cef4db55754b6912b
ruslan <yandiev_97@mail.ru>
2018-02-14 16:06:45 +0300

    Animations disabled on mobile

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 // WOW JS PLUGIN INIT
-wow = new WOW ({
-  boxClass: 'wow',
-  animateClass: 'animated',
-  offset: 0,
-  mobile: false,
-  live: true
-})
-
-new WOW().init();
+var wow = new WOW(
+  {
+    boxClass:     'wow',
+    animateClass: 'animated',
+    offset:       0,
+    mobile:       false,
+    live:         true
+  }
+);
+wow.init();
